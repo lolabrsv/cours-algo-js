ccfc8ff289abe76452a575d7d98582ef15c13b96
lolabrsv <147388611+lolabrsv@users.noreply.github.com>
2023-10-13 11:50:22 +0200

    Add files via upload

--- taxi.js
+++ taxi.js
@@ -14,27 +14,30 @@ class Character {
 
 
 class Trajet {
-    constructor(RedStop, radio, nbrChange) {
-    this.RedStop = 30
+    constructor(redStop, radio, nbrChange) {
+    this.redStop = 30
     this.radio = ["Anissa - Wejdene" , "Back on 74 - Jungle" , "call me - Kali Uchis" , "Nerves - DPR ian", "Ay-yo - NCT 127"]
     this.nbrChange = 0 
 }
-    Backhome(Character) {
-        if(this.RedStop == 0) {
+    Backhome() {
+        if(this.redStop == 0) {
             return true
         }
     }
     MusicRandom(Character) {
     let song = Math.floor(Math.random() * 5)
+    console.log(this.radio[song])
     if(song == 0) {
     Character.hp -= 1
     this.nbrChange += 1
+    console.log("Oh non Anissa ! Je change de taxi")
+    console.log("Aïe ! Santé mentale : " + Character.hp)
     }
-    console.log(this.radio[song])
+    //console.log(this.radio[song])
     }
     RedStop() {
-    this.RedStop -= 1
-    console.log(this.RedStop + " feux rouges restants")
+    this.redStop -= 1
+    console.log(this.redStop + " feux rouges restants")
     }
 }
 
@@ -43,16 +46,16 @@ let trajet = new Trajet ()
 let trajet_on = true 
 
 while(trajet_on == true) {
-    if(John.Backhome(John) == true) {
-    console.log(Character +  " est bien arrivé !")
+    if(trajet.Backhome() == true) {
+    console.log(John.name +  " est bien arrivé !")
     trajet_on = false
+    break
     }
-    if(MentalHealth(John == true)) {
-    console.log(Character + " a explosé")
+    if(John.MentalHealth() == true) {
+    console.log(John.name + " a explosé")
     trajet_on = false
+    break
     }
+    trajet.RedStop()
+    trajet.MusicRandom(John)
    }
-
-
-
-
